5c765e4c00705b97875fd088df246730be82704e
Mark Hellar <mark.hellar@gmail.com>
2017-03-30 17:10:59 -0700

    analog_send

---
+++ 2/code_ie/mqttAnalogSend/mqttAnalogSend.ino
@@ -0,0 +1,79 @@
+// This example uses an Adafruit Huzzah ESP8266
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://shiftr.io/try.
+//
+// by Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <ESP8266WiFi.h>
+#include <MQTTClient.h>
+
+const char *ssid = "extreme";
+const char *pass = "extreme!!!";
+
+char msgBuffer[20];
+const int AnalogIn = A0;
+
+int readingIn = 0;
+
+
+WiFiClient net;
+MQTTClient client;
+
+unsigned long lastMillis = 0;
+
+void connect(); // <- predefine connect() for setup()
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(ssid, pass);
+  client.begin("35.162.196.241", net);
+  connect();
+}
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+//  client.subscribe("/example");
+  // client.unsubscribe("/example");
+}
+
+void loop() {
+  readingIn = analogRead(AnalogIn);
+  Serial.println(readingIn);
+  
+  client.loop();
+  delay(10); // <- fixes some issues with WiFi stability
+
+  if(!client.connected()) {
+    connect();
+  }
+  float inval = readingIn;
+  // publish a message roughly every second.
+  if(millis() - lastMillis > 5) {
+    lastMillis = millis();
+    client.publish("/hello", dtostrf(inval, 6, 2, msgBuffer));
+  }
+}
+
+void messageReceived(String topic, String payload, char * bytes, unsigned int length) {
+  Serial.print("incoming: ");
+  Serial.print(topic);
+  Serial.print(" - ");
+  Serial.print(payload);
+  Serial.println();
+}
