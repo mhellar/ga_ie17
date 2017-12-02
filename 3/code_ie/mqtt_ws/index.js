bfd4947f489ea9e158b54642f47ddf325582e6f8
mhellar <mark.hellar@gmail.com>
2017-12-01 23:25:29 -0800

    commit

--- 3/code_ie/mqtt_ws/index.js
+++ 3/code_ie/mqtt_ws/index.js
@@ -1,10 +1,18 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://35.167.192.176');
-var app = require('express')();
+var client = mqtt.connect('mqtt://10.0.0.193');
+//import express 
+var express = require('express');
+//create express object named app
+var app = express();
 
+//instantiate a server on port 3000
 var server = app.listen(3000);
 var io = require('socket.io')(server);
 
+//expose the local public folder for inluding files js, css etc..
+app.use(express.static('public'));
+
+//on a request to / serve index.html
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -12,7 +20,7 @@ app.get('/', function(req, res) {
 
 client.on('connect', function() {
     client.subscribe('/hello');
-    // client.publish('presence', 'Hello mqtt');
+    // client.publish('/hello', 'Hello mqtt');
 });
 
 client.on('message', function(topic, message) {
